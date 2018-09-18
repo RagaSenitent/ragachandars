7423e8be3d147c39f7a6caffc8e4273b9dc75714
RagaSenitent <42993235+RagaSenitent@users.noreply.github.com>
2018-09-18 11:44:53 +0530

    Delete kc 3.py

--- kc 3.py
+++
@@ -1,8 +0,0 @@
-alpha= input("Input the alphabet: ")
-
-if alpha in ('a', 'e', 'i', 'o', 'u'):
-	print("%s is a vowel." % alpha)
-elif alpha == 'y':
-	print("Sometimes y is vowel or consonant.")
-else:
-	print("%s consonant." % alpha) 
