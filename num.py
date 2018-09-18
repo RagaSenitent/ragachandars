285eaf06c26f8ca80f11fb861ed4b335aeb7312d
RagaSenitent <42993235+RagaSenitent@users.noreply.github.com>
2018-09-18 12:22:33 +0530

    Create num.py

---
+++ num.py
@@ -0,0 +1,5 @@
+num= input("Input the num: ")
+if num in ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'):
+	print("%s Alphabet." % num)
+else:
+	print("No") 
