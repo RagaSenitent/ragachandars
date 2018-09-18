76a967807c6aa55aa68e2268935aeaa74ac6002f
RagaSenitent <42993235+RagaSenitent@users.noreply.github.com>
2018-09-18 14:16:15 +0530

    Update number.py

--- number.py
+++ number.py
@@ -1,10 +1,7 @@
-num1 = 1
-num2 = 3
-num3 = 2
-if (num1 >= num2) and (num1 >= num3):
-   largest = num1
-elif (num2 >= num1) and (num2 >= num3):
-   largest = num2
-else:
-   largest = num3
-print("The largest number is",largest)
+num=[]
+el=int(input("Enter number of elements:"))
+for i in range(1,el+1):
+    b=int(input("Enter element:"))
+    num.append(b)
+num.sort()
+print("Largest element is:",num[el-1])
